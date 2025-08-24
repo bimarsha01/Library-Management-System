c7c85c666d073a69690923c6d32e67dcfd44f526
bimarsha01 <bimarshaghimire09@gmail.com>
2025-08-24 22:42:09 +0545

    used optional

--- src/main/java/org/example/project01lms/Controllers/CustomerController.java
+++ src/main/java/org/example/project01lms/Controllers/CustomerController.java
@@ -1,22 +1,28 @@
 package org.example.project01lms.Controllers;
 
+import org.example.project01lms.Converter.CustomerConverter;
 import org.example.project01lms.Dto.CustomerDto;
+import org.example.project01lms.Models.Customers;
+import org.example.project01lms.Repo.CustomerRepo;
 import org.example.project01lms.Response.ApiResponse;
 import org.example.project01lms.Services.CustomerServices.CustomerService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController  extends BaseController{
 
     public final CustomerService customerService;
+    private final CustomerRepo customerRepo;
+    private  final CustomerConverter customerConverter;
 
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, CustomerRepo customerRepo, CustomerConverter customerConverter) {
         this.customerService = customerService;
+        this.customerRepo = customerRepo;
+        this.customerConverter = customerConverter;
     }
 
 
--- src/main/java/org/example/project01lms/Repo/CustomerRepo.java
+++ src/main/java/org/example/project01lms/Repo/CustomerRepo.java
@@ -3,6 +3,9 @@ package org.example.project01lms.Repo;
 import org.example.project01lms.Models.Customers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepo extends JpaRepository<Customers, Long> {
     boolean existsByLibraryId(String randomLid);
+    Optional<Customers> findByLibraryId(String libraryId);
 }
